6861f1eae6e316809e2d02c54ca32b771a6d3082
watcharakorn996 <45086249+watcharakorn996@users.noreply.github.com>
2018-11-21 22:14:18 -0800

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ $access_token = '27dSrRPH63AnxpqY48cpWMSNxVo/Pv3BNkaF5H8XHCLMV9Xod60XJo405M/k1sk
 
 $channelSecret = '697d490071792456ccb928cbc210b051';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U72b08ce02bea46dc8d252a181e0495c2';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
