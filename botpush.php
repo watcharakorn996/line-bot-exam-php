7114ec436326a95c32a988a1e9c7bd492248d1c2
watcharakorn996 <45086249+watcharakorn996@users.noreply.github.com>
2018-11-21 22:02:44 -0800

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,9 +4,9 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '27dSrRPH63AnxpqY48cpWMSNxVo/Pv3BNkaF5H8XHCLMV9Xod60XJo405M/k1skfRuxDHALIWaLJC+bZ3V1SHo7RuexdE1dymM4NR8N8/WmLdpbBBPx8tRdXjuAhplQOUDnO0FIZktlUUoo1VYIRxAdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '697d490071792456ccb928cbc210b051';
 
 $pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
 
